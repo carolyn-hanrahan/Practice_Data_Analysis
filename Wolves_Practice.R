eec1791383f1df44e2166133accab473865a7643
Carolyn Hanrahan <carolyn.hanrahan23@gmail.com>
2024-02-09 13:26:39 -0500

    continue practice script

--- Wolves_Practice.R
+++ Wolves_Practice.R
@@ -9,7 +9,7 @@ library(tidyr)
 library(tidyverse)
 library(dplyr)
 
-read_csv("Wolves_Filtered_100.xlsx")
+read_xlsx("Wolves_Filtered_100.xlsx")
 
 data <- Wolves_Filtered_100
 view(data)
@@ -19,6 +19,7 @@ view(data)
 ## Let's create a dataframe because that seems like a good idea. 
 DietItem <- data$...1
 ReadCount <- data$...2
+
 
 DataFrame <- data.frame(DietItem, ReadCount)
 
@@ -54,24 +55,45 @@ ggplot(data=DataFrame) +
 
 set.seed(123)
 
-DataFrame$Season <- sample(1:3)
+DataFrame$ReadCount2 <- runif(39, min=0, max=30000)
 
 print(DataFrame)
 view(DataFrame)
 
+DataFrame$ReadCount3 <- runif(39, min=0, max=30000)
 
+view(DataFrame)
 
-value_counts <- table(DataFrame$Season)
-barplot(value_counts)
+#value_counts <- table(DataFrame$Season)
+#barplot(value_counts)
 
-pie(value_counts) # this shows that there are basically the same number of samples per season 
+#pie(value_counts) # this shows that there are basically the same number of samples per season 
 
 
 ## Calculating ANOVA! 
 
 # note to self: okay I think I need to recreate this dataframe so that there is a column for each season rather than a "season" column...? 
 
-anova_result <- aov(ReadCount[] ~ Season, data=DataFrame)
+anova_result <- aov(ReadCount ~ ReadCount2, data=DataFrame)
 
-print(anova_result)
+summary(anova_result) #A larger F-value suggests that the variance between groups is larger compared to the variance within groups, which could indicate a significant difference in means.
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# I will now attempt to create a MDS plot for similarity/dissimilarity 
+
+mds_data <- DataFrame[, -1]
+
+# dissimilarity matrix: 
+
+dissim_matrix <- dist(mds_data)
+
+
+# MDS
+mds_result <- cmdscale(dissim_matrix)
+
+#plot
+plot(mds_result, type= "n", xlab= "Dimension 1", ylab= "Dimension 2")
+
+
 
