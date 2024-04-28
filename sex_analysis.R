9be7cb4413e21929f8045d62f21345a402fa5ab8
Carolyn Hanrahan <carolyn.hanrahan23@gmail.com>
2024-04-28 08:07:52 -0400

    sex analysis

---
+++ sex_analysis.R
@@ -0,0 +1,135 @@
+## Metabarcoding Statistical Analysis -- Coyote Sex
+## April 25, 2024
+## Carolyn Hanrahan 
+
+# Load in packages 
+library(vegan)
+library(ggplot2)
+library(tidyr)
+library(tidyverse)
+library(dplyr)
+library(ggrepel)
+library(ggbiplot)
+library(ggplot2)
+library(ggalt)
+library(ggforce)
+library(ggforce)
+library(vegan)
+library(rJava)
+library(xlsx)
+library(openxlsx)
+#library(MASS)
+
+# Install and load the readxl package
+install.packages("readxl")
+library(readxl)
+
+# Read the Excel file/Import data into R: 
+sex_data <- read_excel("coyote_sex_data.xlsx")
+
+sex_data <- sex_data %>% select(c('Interpretation (Diet item)', 'S23_0476':'S23_2874'))
+
+sex_data <- sex_data[1:30,]
+
+
+sex_data_longer <- sex_data %>%
+  pivot_longer(cols = 'S23_0476':'S23_2874',
+               names_to = "Sample",
+               values_to = "No_Reads")
+
+
+
+sex_data_wider <- sex_data_longer %>%
+  pivot_wider(names_from = "Interpretation (Diet item)",
+              values_from = "No_Reads")
+
+
+# Split the list column into two separate columns
+sex_data_wider <- unnest_wider(sex_data_wider, "NA", names_sep = "_")
+
+# Rename the last two columns
+sex_data_wider$Year <- sex_data_wider$NA_1
+
+sex_data_wider$CoyoteID <- sex_data_wider$NA_2
+
+sex_data_wider$Sex <- sex_data_wider$NA_3
+
+# Remove two columns in the middle
+sex_data_wider <- sex_data_wider[, -c(29:31)]
+
+sex_data <- sex_data_wider
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# turn data into presence/absence data to calculate "species richness" 
+
+sex_data <- as.data.frame(sex_data)
+
+
+for(i in 2:28){
+  sex_data[,i] <- as.numeric(sex_data[,i])
+} 
+
+for(i in 2:28){
+  for (j in 1:nrow(sex_data)){
+    sex_data[j,i] <- ifelse(sex_data[j,i] >0, "1", "0")
+  }
+}
+
+
+for(i in 2:28){
+  sex_data[,i] <- as.numeric(sex_data[,i])
+} 
+
+sex_data$SpeciesRichness <- rowSums(sex_data[,2:28])
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Subset male and female 
+
+
+female <- sex_data %>%
+  filter(Sex == "F")
+
+male <- sex_data %>%
+  filter(Sex == "M")
+
+
+
+#__________________________________________________________________________________
+# Calculate FOO for diet type for female/male 
+
+# female 
+total_f <- colSums(female[,2:28])
+
+divisor <- 75
+
+total_FOO_female <- total/divisor
+
+# Rename columns
+colnames(total_FOO_female) <-  "FOO"
+
+total_FOO_female$diet_item <- row.names(total_FOO_female)
+
+# Ordered bar plot 
+
+ggplot(total_FOO_female, aes(x = reorder(diet_item, -FOO), y = FOO)) +
+  geom_bar(stat = "identity", fill = "magenta") +
+  labs(title = "Prevalence of Diet Items (Female)", x = "Diet Item", y = "FOO") +  # Add plot title
+  coord_cartesian(ylim = c(0, 0.7))  # Set y-axis limits
+theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
